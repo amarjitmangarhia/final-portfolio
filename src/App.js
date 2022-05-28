a3398572c605152d6486da6a7287a11a9c26c435
Amarjit <amarjitmangarhia@gmail.com>
2022-05-28 16:22:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import myImg from "./img/finallll.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <header id="header">
+          <div className="hello-text">Hello I Am</div>
+          <div className="navbar">
+            <div className="nav-items">Home</div>
+            <div className="nav-items">About</div>
+            <div className="nav-items">Project</div>
+            <div className="nav-items ">Contact</div>
+          </div>
+        </header>
+        <main id="main">
+          <div className="left">
+            <div className="wrapper">
+              <img className="img" src={myImg} alt="" />
+            </div>
+          </div>
+          <div className="right">
+            <div className="first-name">amar</div>
+            <div className="last-name">mangarhia</div>
+          </div>
+        </main>
+        <footer id="footer">
+          <div className="mern">MERN STACK DEV</div>
+          <div className="skills">MONGO DB | EXPRESS | REACT | NODE JS</div>
+        </footer>
+      </div>
     </div>
   );
 }
