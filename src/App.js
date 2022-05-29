4229d98da592382e8b628d64d4d136e4d5efb9bb
Amarjit <amarjitmangarhia@gmail.com>
2022-05-29 13:53:48 +0530

    gjrkhkr

--- src/App.js
+++ src/App.js
@@ -3,24 +3,58 @@ import { useState } from "react";
 import "./App.css";
 import myImg from "./img/finallll.png";
 function App() {
-  const [first, setfirst] = useState(true);
+  const [showHamburger, setShowHamburger] = useState(true);
+  const [showClose, setShowClose] = useState(false);
 
   const onClickHandler = () => {
-    setfirst((prevState) => !prevState);
+    setShowHamburger((prevState) => !prevState);
+    setShowClose((prevState) => !prevState);
   };
 
-  let content = "";
+  let leftNavbarClass = "";
 
-  if (first) {
-    content = "left-navbar";
+  if (showHamburger) {
+    leftNavbarClass = "left-navbar";
   } else {
-    content = "left-navbar df";
+    leftNavbarClass = "left-navbar df";
+  }
+
+  let hamburgerClose = "";
+
+  if (!showClose) {
+    hamburgerClose = (
+      <svg
+        onClick={onClickHandler}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        className="svgHamburger"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fillRule="evenodd"
+          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    );
+  } else {
+    hamburgerClose = (
+      <svg
+        onClick={onClickHandler}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        className="svgClose"
+        viewBox="0 0 16 16"
+      >
+        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+      </svg>
+    );
   }
 
   return (
     <div>
       <div className="container">
-        <div className={content}>
+        <div className={leftNavbarClass}>
           <div className="left-nav-items">
             <a href="google.com">Home</a>
           </div>
@@ -43,18 +77,7 @@ function App() {
             <div className="nav-items">Project</div>
             <div className="nav-items ">Contact</div>
           </div>
-          <svg
-            onClick={onClickHandler}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            className="svgHamburger"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-            />
-          </svg>
+          {hamburgerClose}
         </header>
         <main id="main">
           <div className="left">
@@ -63,7 +86,7 @@ function App() {
             </div>
           </div>
           <div className="right">
-            <div className="first-name">amar</div>
+            <div className=" first-name">amar</div>
             <div className="last-name">mangarhia</div>
           </div>
         </main>
