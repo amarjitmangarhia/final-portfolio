03a8422687a4e256c029790c5f5fcba46787bbe3
Amarjit <amarjitmangarhia@gmail.com>
2022-06-01 11:00:36 +0530

    bmfkkfd

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,31 +6,37 @@ import wheelChair from "../../img/wheelChair.svg";
 const About = () => {
   return (
     <div className={classes.about}>
-      <div className={classes.left}>
-        <div className={classes.header}>Who Am I</div>
-        <div className={classes.desc}>
-          Amarjit, A Computer Science Graduate.
-          <br />
-          Fully self taught.
-          <img
-            className={classes.wheelChair}
-            src={wheelChair}
-            alt="wheelChair"
-          />
+      <div className="container">
+        <div className={classes.left}>
+          <div className={classes.header}>Who Am I</div>
+          <div className={classes.desc}>
+            Amarjit, A Computer Science Graduate.
+            <br />
+            Fully self taught.
+            <div className={classes.image}>
+              <img
+                className={classes.wheelChair}
+                src={wheelChair}
+                alt="wheelChair"
+              />
+            </div>
+          </div>
         </div>
       </div>
-      <div className={classes.right}>
-        <div className={classes.rightNav}>Skills I Have</div>
-        <div className={classes.image}>
-          <img className={classes.img} src={boyToy} alt="myImage" />
-        </div>
-        <div className={classes.skills}>
-          <div className={classes.skill}>HTML</div>
-          <div className={classes.skill}>CSS</div>
-          <div className={classes.skill}>JAVASCRIPT</div>
-          <div className={classes.skill}>NODE JS</div>
-          <div className={classes.skill}>REACT</div>
-          <div className={classes.skill}>UI/UX</div>
+      <div className="container">
+        <div className={classes.right}>
+          <div className={classes.rightNav}>Skills I Have</div>
+          <div className={classes.image}>
+            <img className={classes.img} src={boyToy} alt="myImage" />
+          </div>
+          <div className={classes.skills}>
+            <div className={classes.skill}>HTML</div>
+            <div className={classes.skill}>CSS</div>
+            <div className={classes.skill}>JAVASCRIPT</div>
+            <div className={classes.skill}>NODE JS</div>
+            <div className={classes.skill}>REACT</div>
+            <div className={classes.skill}>UI/UX</div>
+          </div>
         </div>
       </div>
     </div>
