6a3fbe9fc68fdfa986d0ea07fce8fdd6458f8734
Amarjit <amarjitmangarhia@gmail.com>
2022-06-01 11:17:29 +0530

    bg

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -4,15 +4,17 @@ import classes from "./Main.module.css";
 
 const Main = () => {
   return (
-    <main className={classes.main}>
-      <div className={classes.left}>
-        <img className={classes.img} src={myImg} alt="myImage" />
-      </div>
-      <div className={classes.right}>
-        <div className={classes.firstName}>amar</div>
-        <div className={classes.lastName}>mangarhia</div>
-      </div>
-    </main>
+    <div className="container">
+      <main className={classes.main}>
+        <div className={classes.left}>
+          <img className={classes.img} src={myImg} alt="myImage" />
+        </div>
+        <div className={classes.right}>
+          <div className={classes.firstName}>amar</div>
+          <div className={classes.lastName}>mangarhia</div>
+        </div>
+      </main>
+    </div>
   );
 };
 
